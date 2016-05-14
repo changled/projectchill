062291b84e4a08724238da48614ddbb265e1239a
Henry Bowman <hbowman@calpoly.edu>
2016-05-14 15:10:16 -0700

    Adding user object

---
+++ user.py
@@ -0,0 +1,14 @@
+class User(object):
+   firstName = ""
+   lastName = ""
+   email = ""
+   password = ""
+   phoneNumber = ""
+   ssn = 0
+   finance = ""
+   emplID = 0
+   timeCard = ""
+   clearance = 0
+   position = ""
+   jobType = ""   
+   
